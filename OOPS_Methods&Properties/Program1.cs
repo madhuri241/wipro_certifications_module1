dc376a5c89e9e881bfe10c784cf0255ed3cb0c88
madhuri <madhuri101004@gmail.com>
2025-07-15 11:42:45 +0530

    oops

---
+++ OOPS_Methods&Properties/Program1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+public class Employee
+{
+    string emp_Name;
+    double basic_Salary,HRA,DA,TAX,gross_Pay,net_Salary;
+    public Employee(string emp_Name, double basic_Salary)
+    {
+        this.emp_Name = emp_Name;
+        this.basic_Salary = basic_Salary;
+        
+    }
+    public void CalculateSalary()
+    {
+        HRA = basic_Salary * 0.15;
+        DA = basic_Salary * 0.10;
+        gross_Pay = basic_Salary + HRA + DA;
+        TAX = gross_Pay * 0.08;
+        net_Salary = gross_Pay - TAX;
+    }
+    public void DisplaySalary()
+    {
+        Console.WriteLine("Employee Name: " + emp_Name);
+        Console.WriteLine("Basic Salary: " + basic_Salary);
+        Console.WriteLine("HRA: " + HRA);
+        Console.WriteLine("DA: " + DA);
+        Console.WriteLine("Gross Pay: " + gross_Pay);
+        Console.WriteLine("TAX: " + TAX);
+        Console.WriteLine("Net Salary: " + net_Salary);
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter Employee Name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter Basic Salary: ");
+        double salary = Convert.ToDouble(Console.ReadLine());
+        Employee emp = new Employee(name, salary);
+        emp.CalculateSalary();
+        emp.DisplaySalary();
+    }
+}
---
+++ OOPS_Methods&Properties/Program2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Methods_Properties
+{
+   public class Stock
+    {
+        string stockName;
+        string stockSymbol;
+        double prevstockPrice;
+        double currentStockPrice;
+        public Stock(string stockName, string stockSymbol, double prevstockPrice, double currentStockPrice)
+        {
+            this.stockName = stockName;
+            this.stockSymbol = stockSymbol;
+            this.prevstockPrice = prevstockPrice;
+            this.currentStockPrice = currentStockPrice;
+        }
+        public void getChangePercentage()
+        {
+            double changePercentage = ((currentStockPrice - prevstockPrice) / prevstockPrice) * 100;
+            Console.WriteLine($"The change percentage for {stockName} ({stockSymbol}) is {changePercentage:F2}%");
+        }
+    }
+    public class Program2
+    {
+       static void Main(string[] args)
+        {
+            Stock stock1 = new Stock("Apple Inc.", "AAPL", 150.00, 155.00);
+            stock1.getChangePercentage();
+            Stock stock2 = new Stock("Microsoft Corp.", "MSFT", 280.00, 275.00);
+            stock2.getChangePercentage();
+            
+        }
+    }
+}
